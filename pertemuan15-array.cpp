f39bf7c846fd168b1fe5d623495c5ef418866bf9
CTA-Dev29 <chevyalamsyach6@gmail.com>
2025-05-21 10:17:05 +0700

    Create pertemuan15-array.cpp

---
+++ pertemuan15-array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai[5] = {80, 75, 90, 85, 70};
+
+    cout << "Nilai Mahasiswa: ";
+    for (int i = 0; i < 5; i++) {
+        cout << nilai[i] << " ";
+    }
+
+    return 0;
+}
