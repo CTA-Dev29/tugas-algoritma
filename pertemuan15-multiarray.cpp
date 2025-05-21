af90f8c083d5139c60ba8aa794dd76990fa2f809
CTA-Dev29 <chevyalamsyach6@gmail.com>
2025-05-21 10:18:41 +0700

    Create pertemuan15-multiarray.cpp

---
+++ pertemuan15-multiarray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matriks[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    cout << "Isi Matriks:\n";
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matriks[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
