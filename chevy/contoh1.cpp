80612bc2b03df7b0123ed5daa2d1550f60fa25b0
Chevy Alamsyach <chevyalamsyach6@gmail.com>
2025-06-11 19:27:53 +0700

    Add files via upload

---
+++ chevy/contoh1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+garis()
+{ 
+printf("\n----------------------\n");
+}
+/* program utama */
+int main(){
+
+garis(); //memanggil fungsi garis
+cout<<"AMIK BSI - Pondok Labu"<<endl;
+garis(); //memanggil
+}
---
+++ chevy/penjumlahan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int tambah(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Hasil: " << tambah(3, 5);
+    return 0;
+}
+
